056e9b67218fa231f65e64da18ea9491dd491c28
Andy Mina <mina.andy54@gmail.com>
2022-08-08 21:10:48 -0400

    add starter code

---
+++ init-firebase.js
@@ -0,0 +1,10 @@
+/**
+ * @TODO replace the firebaseConfig object below with the one 
+ * provided for you by firebase after creating your project!
+*/
+const firebaseConfig = {
+    
+};
+
+// initialize the firebase app with this built-in function
+firebase.initializeApp(firebaseConfig);
---
+++ main.js
@@ -0,0 +1,66 @@
+/**
+ * @TODO get a reference to the Firebase Database object
+ */
+
+/**
+ * @TODO get const references to the following elements:
+ *      - div with id #all-messages
+ *      - input with id #username
+ *      - input with id #message
+ *      - button with id #send-btn and the updateDB
+ *        function as an onclick event handler
+ */
+
+/**
+ * @TODO create a function called updateDB which takes
+ * one parameter, the event, that:
+ *      - gets the values of the input elements and stores
+ *        the data in a temporary object with the keys USERNAME
+ *        and MESSAGE
+ *      - console.logs the object above
+ *      - writes this object to the database
+ *      - resets the value of #message input element
+ */
+
+/**
+ * @TODO add the addMessageToBoard function as an event
+ * handler for the "child_added" event on the database
+ * object
+ */
+
+/**
+ * @TODO create a function called addMessageToBoard that
+ * takes one parameter rowData which:
+ *      - console.logs the data within rowData
+ *      - creates a new HTML element for a single message
+ *        containing the appropriate data
+ *      - appends this HTML to the div with id
+ *        #all-messages (we should have a reference already!)
+ * 
+ */
+
+/** 
+ * @TODO create a function called makeSingleMessageHTML which takes
+ * two parameters, usernameTxt and messageTxt, that:
+ *      - creates a parent div with the class .single-message
+ * 
+ *      - creates a p tag with the class .single-message-username
+ *      - update the innerHTML of this p to be the username 
+ *        provided in the parameter object
+ *      - appends this p tag to the parent div
+ * 
+ *      - creates a p tag
+ *      - updates the innerHTML of this p to be the message
+ *        text provided in the parameter object
+ *      - appends this p tag to the parent div
+ * 
+ *      - returns the parent div
+ */
+
+/**
+ * @BONUS add an onkeyup event handler to the form HTML
+ * element so the user can also submit the form with the
+ * Enter key
+ * 
+ * @BONUS use an arrow function
+ */
