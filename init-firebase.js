b3463185b8da88d1e9cd96d3400b00ef18314573
Tony <tjwang4@gmail.com>
2022-08-09 12:42:01 -0700

    created chatbox

--- init-firebase.js
+++ init-firebase.js
@@ -1,9 +1,15 @@
 /**
- * @TODO replace the firebaseConfig object below with the one 
+ * @TODO replace the firebaseConfig object below with the one
  * provided for you by firebase after creating your project!
-*/
+ */
 const firebaseConfig = {
-    
+  apiKey: "AIzaSyDFKYGmI4v07K1N5UQBd-xZohmLBwDnaqU",
+  authDomain: "firemessages-ca646.firebaseapp.com",
+  databaseURL: "https://firemessages-ca646-default-rtdb.firebaseio.com",
+  projectId: "firemessages-ca646",
+  storageBucket: "firemessages-ca646.appspot.com",
+  messagingSenderId: "911910241882",
+  appId: "1:911910241882:web:90582a0a5895674472ba63",
 };
 
 // initialize the firebase app with this built-in function
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * @TODO get a reference to the Firebase Database object
  */
+const database = firebase.database().ref();
 
 /**
  * @TODO get const references to the following elements:
@@ -10,6 +11,11 @@
  *      - button with id #send-btn and the updateDB
  *        function as an onclick event handler
  */
+const allMessages = document.querySelector("#all-messages");
+const usernameElem = document.querySelector("#username");
+const messageElem = document.querySelector("#message");
+const sendBtn = document.querySelector("#send-btn");
+sendBtn.onclick = updateDB;
 
 /**
  * @TODO create a function called updateDB which takes
@@ -21,12 +27,32 @@
  *      - writes this object to the database
  *      - resets the value of #message input element
  */
+function updateDB(event) {
+  // prevent default form behavior
+  event.preventDefault();
+
+  // make a temp object called data as described in the @TODO
+  let data = {
+    USERNAME: usernameElem.value,
+    MESSAGE: messageElem.value,
+  };
+
+  // print for good measure
+  console.log(data);
+
+  // push data object to the database
+  database.push(data);
+
+  // reset message value
+  messageElem.value = "";
+}
 
 /**
  * @TODO add the addMessageToBoard function as an event
  * handler for the "child_added" event on the database
  * object
  */
+database.on("child_added", addMessageToBoard);
 
 /**
  * @TODO create a function called addMessageToBoard that
@@ -36,31 +62,70 @@
  *        containing the appropriate data
  *      - appends this HTML to the div with id
  *        #all-messages (we should have a reference already!)
- * 
+ *
  */
+function addMessageToBoard(rowData) {
+  console.log(rowData);
 
-/** 
+  // get the "object form" of the data passed from firebase
+  let data = rowData.val();
+
+  // print for good measure
+  console.log(data);
+
+  // get the singleMessageElem
+  let singleMessage = makeSingleMessageHTML(data.USERNAME, data.MESSAGE);
+
+  // append singleMessage to #all-messages
+  allMessages.append(singleMessage);
+}
+/**
  * @TODO create a function called makeSingleMessageHTML which takes
  * two parameters, usernameTxt and messageTxt, that:
  *      - creates a parent div with the class .single-message
- * 
+ *
  *      - creates a p tag with the class .single-message-username
- *      - update the innerHTML of this p to be the username 
+ *      - update the innerHTML of this p to be the username
  *        provided in the parameter object
  *      - appends this p tag to the parent div
- * 
+ *
  *      - creates a p tag
  *      - updates the innerHTML of this p to be the message
  *        text provided in the parameter object
  *      - appends this p tag to the parent div
- * 
+ *
  *      - returns the parent div
  */
+function makeSingleMessageHTML(usernameTxt, messageTxt) {
+  // create a parent div
+  let parentDiv = document.createElement("div");
+  // add a .single-message class to this div
+  parentDiv.setAttribute("class", "single-message");
+
+  // create a p tag called usernameP
+  let usernameP = document.createElement("p");
+  // add a .single-message-username class to this p
+  usernameP.classList.add("single-message-username");
+  // update the innerHTML of this p tag to the appropriate date
+  usernameP.innerHTML = usernameTxt + ":";
+  // append this p tag to the parentDiv
+  parentDiv.append(usernameP);
+
+  // create a p tag called messageP
+  let messageP = document.createElement("p");
+  // update the innerHTML of the p tag to the appropriate data
+  messageP.innerHTML = messageTxt;
+  // append this p tag to the parentDiv
+  parentDiv.append(messageP);
+
+  // return the parentDiv
+  return parentDiv;
+}
 
 /**
  * @BONUS add an onkeyup event handler to the form HTML
  * element so the user can also submit the form with the
  * Enter key
- * 
+ *
  * @BONUS use an arrow function
  */
